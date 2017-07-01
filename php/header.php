e7bf00bd22f6372cfc2567a23e7e9c909697cc7b
Bernard <bernardy_ho@hotmail.com>
2017-07-01 17:48:39 +0800

    Add files via upload

---
+++ php/header.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Server Maintenance | Spare Part Specialist - Infra Hutama Solusindo</title>
+
+  <meta name="description" content="INFRA HUTAMA SOLUSINDO (IHS) is a company that maintain server. Server maintenance is the process of keeping the server updated and running to ensure that the organization's computer network is operating smoothly. The network administrator usually does this, and it is crucial to the performance of the business.">
+  <meta name="keywords" content="Server Maintenance, Server, Storage, Maintenance, Spare Part, Server Spare Part, Hardware">
+
+<link rel="stylesheet" href="./vendors/icon-socicon/css/socicon.css">
+<link rel="stylesheet" href="./vendors/icon-linearicon/css/icon-linearicon.css">
+<link rel="stylesheet" href="./vendors/icon-sosa/css/icon-sosa.css">
+<link rel="stylesheet" href="./vendors/icon-lineabasic/css/icon-lineabasic.css">
+<link rel="stylesheet" href="./vendors/icon-simpleline/css/icon-simple-line.css">
+<link rel="stylesheet" href="./css/style.css">
+<link rel="shortcut icon" href="images/favicon.ico.png" type="image/x-icon" />
+
+
+</head>
+<body>
+  <div class="main-wrapper">
+  <header>
+    	<div id="logo">
+        <a href="index.html">
+          <img src="images/IHS Design2 v.02" alt="">
+        </a>
+      </div>
+
+      <div class="header-menu">
+      	<ul>
+        	<li><a href="index.html">Home</a></li>
+        	<li><a href="about-us.html">About Us</a></li>
+          <li><a href="expertise.html">Expertise</a></li>
+          <li><a href="contact-us.html">Contact Us</a></li>
+        </ul>
+      </div>
+
+    <!---THREE PANEL--->
+    <div class="three-panel">
+      <div class="panel">      </div>
+      <div class="panel">      </div>
+      <div class="panel">      </div>
+    </div>
+
+  </header>
